7071a6035a7ba81f4267372e42f21b6c87b988db
Jacob Araiza <jacob@mlabs.city>
2022-08-22 17:22:44 +0100

    Split sending and receiving messages in client

--- client/src/main.rs
+++ client/src/main.rs
@@ -14,10 +14,14 @@ mod encrypt;
 fn main() {
     let args = Arguments::from_args();
     let client = RpcClient::new_with_commitment(args.url, CommitmentConfig::confirmed());
-    let encryption_key = encrypt::SharedKey::new(&args.keypair, &args.to);
-    let ciphertext = encryption_key.transmit_key.encrypt(&args.message);
-    send_direct_mesage(&client, &args.keypair, &args.to, ciphertext).unwrap();
-    print_messages(&client, &args.keypair);
+    match args.command {
+        Command::Send { to, message } => send(&client, &args.keypair, &to, &message).unwrap(),
+        Command::GetUnread => {
+            for message in all_messages(&client, &args.keypair.pubkey()) {
+                println!("From {}: {}", message.from, message.decrypt(&args.keypair));
+            }
+        }
+    }
 }
 
 #[derive(StructOpt)]
@@ -26,28 +30,34 @@ struct Arguments {
     url: String,
     #[structopt(short, long, parse(try_from_str=read_keypair_file), default_value = "~/.chat.keypair")]
     keypair: Keypair,
-    #[structopt(short, long)]
-    to: Pubkey,
-    #[structopt(short, long)]
-    message: String,
+    #[structopt(subcommand)]
+    command: Command,
 }
 
-fn send_direct_mesage(
-    client: &RpcClient,
-    sender: &Keypair,
-    receiver: &Pubkey,
-    encrypted_text: Vec<u8>,
-) -> Result<(), Error> {
+#[derive(StructOpt)]
+enum Command {
+    Send {
+        #[structopt(short, long)]
+        to: Pubkey,
+        #[structopt(short, long)]
+        message: String,
+    },
+    GetUnread,
+}
+
+fn send(client: &RpcClient, from: &Keypair, to: &Pubkey, message: &str) -> Result<(), Error> {
+    let key = encrypt::SharedKey::new(from, to);
+    let ciphertext = key.transmit_key.encrypt(message);
     let message_seed: [u8; 8] = rand::thread_rng().gen();
     let (message_pda, _bump) = Pubkey::find_program_address(&[&message_seed], &program::ID);
     let instruction = program::send_direct_mesage(
-        sender.pubkey(),
-        *receiver,
+        from.pubkey(),
+        *to,
         message_seed.into(),
         message_pda,
-        encrypted_text,
+        ciphertext,
     );
-    execute(client, sender, &[instruction], vec![sender])
+    execute(client, from, &[instruction], vec![from])
 }
 
 fn execute(
@@ -67,21 +77,43 @@ fn execute(
     Ok(())
 }
 
-fn print_messages(client: &RpcClient, me: &Keypair) {
-    let mailbox_pda = program::mailbox_pda(&me.pubkey());
-    let mailbox_account = client.get_account(&mailbox_pda).expect("No messages");
-    let mailbox = program::Mailbox::try_deserialize(&mut mailbox_account.data.as_ref())
-        .expect("Not a Mailbox account");
-    let mut next_message = mailbox.inbox;
-    while let Some(message_pda) = next_message {
+#[derive(serde::Serialize, serde::Deserialize)]
+struct State {
+    read_messages: Option<Pubkey>,
+}
+
+fn all_messages<'a, 'b>(
+    client: &'a RpcClient,
+    to: &'b Pubkey,
+) -> impl Iterator<Item = program::Message> + 'a {
+    let mailbox_pda = program::mailbox_pda(to);
+    let mut next_message = client
+        .get_account_with_commitment(&mailbox_pda, CommitmentConfig::confirmed())
+        .expect("Could not fetch account")
+        .value
+        .and_then(|mailbox_account| {
+            program::Mailbox::try_deserialize(&mut mailbox_account.data.as_ref())
+                .expect("Not a Mailbox account")
+                .inbox
+        });
+    std::iter::from_fn(move || {
         let message_account = client
-            .get_account(&message_pda)
+            .get_account(&next_message?)
             .expect("Message PDA not found");
         let message = program::Message::try_deserialize(&mut message_account.data.as_ref())
             .expect("Not a Message account");
-        let key = encrypt::SharedKey::new(me, &message.from);
-        let text = key.receive_key.decrypt(&message.ciphertext);
-        println!("From {}: {}", message.from, text);
-        next_message = message.inbox
+        next_message = message.inbox;
+        Some(message)
+    })
+}
+
+trait Decrypt {
+    fn decrypt(&self, to: &Keypair) -> String;
+}
+
+impl Decrypt for program::Message {
+    fn decrypt(&self, to: &Keypair) -> String {
+        let key = encrypt::SharedKey::new(to, &self.from);
+        key.receive_key.decrypt(&self.ciphertext)
     }
 }
